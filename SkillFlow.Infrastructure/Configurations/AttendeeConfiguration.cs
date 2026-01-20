0ed75d2ebe94266af3903f44f5b0c40675131f0d
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-01-20 22:04:44 +0100

    Adds Attendee configuration

---
+++ SkillFlow.Infrastructure/Configurations/AttendeeConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillFlow.Domain.Attendees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillFlow.Infrastructure.Configurations
+{
+    public class AttendeeConfiguration : IEntityTypeConfiguration<Attendee>
+    {
+        public void Configure(EntityTypeBuilder<Attendee> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id)
+                .HasConversion(id => id.Value, v => new AttendeeId(v));
+
+            builder.Property(a => a.Email)
+                .HasConversion(e => e.Value, v => Email.Create(v))
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.HasIndex(a => a.Email).IsUnique();
+
+            builder.ComplexProperty(a => a.Name, name =>            {
+                name.Property(p => p.FirstName)
+                .HasColumnName("FirstName")
+                .IsRequired()
+                .HasMaxLength(150);
+
+                name.Property(p => p.LastName)
+                .HasColumnName("LastName")
+                .IsRequired()
+                .HasMaxLength(150);
+            });
+
+            builder.Property(a => a.PhoneNumber)
+                .HasConversion(p => p.HasValue ? p.Value.Value : null, v => PhoneNumber.Create(v))
+                .IsRequired(false);
+
+            builder.HasDiscriminator(a => a.Role)
+                .HasValue<Student>(Role.Student)
+                .HasValue<Instructor>(Role.Instructor);
+        }
+    }
+}
--- SkillFlow.Presentation/Program.cs
+++ SkillFlow.Presentation/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SkillFlow.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<SkillFlowDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString(""));
+});
 
 var app = builder.Build();
 
