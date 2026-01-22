b097ff60e07fb63c620abda8eb619e6763485f02
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-01-22 17:01:15 +0100

    Adds ICourseRepository

---
+++ SkillFlow.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using SkillFlow.Domain.Courses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillFlow.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<Course?> GetByIdAsync(CourseId id);
+        Task<Course?> GetByCourseCodeAsync(CourseCode code);
+
+        Task<IEnumerable<Course>> GetAllAsync();
+        Task<IEnumerable<Course>> SearchByNameAsync(CourseName searhTerm);
+
+        Task AddAsync(Course course);
+        Task UpdateAsync(Course course);
+        Task DeleteAsync(CourseId id);
+
+    }
+}
