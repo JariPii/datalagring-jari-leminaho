936b175d74a7fccd35d9f73469b103c5939a06b4
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-01-22 03:52:02 +0100

    Adds Enrollment Configuration

---
+++ SkillFlow.Infrastructure/Configurations/EnrollmentConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillFlow.Domain.Attendees;
+using SkillFlow.Domain.CourseSessions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillFlow.Infrastructure.Configurations
+{
+    public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
+    {
+        public void Configure(EntityTypeBuilder<Enrollment> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id)
+                .HasConversion(id => id.Value, v => new EnrollmentId(v));
+
+            builder.Property(e => e.Status)
+                .HasConversion<string>()
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(e => e.StudentId)
+                .HasConversion(id => id.Value, v => new AttendeeId(v));
+
+            builder.Property(e => e.CourseSessionId)
+                .HasConversion(id => id.Value, v => new CourseSessionId(v));
+
+            builder.HasOne(e => e.Student)
+                .WithMany()
+                .HasForeignKey(e => e.StudentId);
+
+            builder.HasOne(e => e.CourseSession)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.CourseSessionId);
+        }
+    }
+}
