b4e8c098bb21a7cb7f93e7eec794c6c3a4701928
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-02-19 20:31:32 +0100

    Adds DI files for Infrastructure and Presentation

---
+++ SkillFlow.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SkillFlow.Domain.Interfaces;
+using SkillFlow.Infrastructure.Persistence;
+using SkillFlow.Infrastructure.Repositories;
+
+namespace SkillFlow.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<SkillFlowDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+            services.AddScoped<IAttendeeQueries, AttendeeRepository>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ICourseSessionRepository, CourseSessionRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<ICompetenceRepository, CompetenceRepository>();
+
+            return services;
+        }
+    }
+}
--- SkillFlow.Presentation/Program.cs
+++ SkillFlow.Presentation/Program.cs
@@ -1,78 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using SkillFlow.Application;
-using SkillFlow.Application.Interfaces;
-using SkillFlow.Application.Services.Attendees;
-using SkillFlow.Application.Services.Competences;
-using SkillFlow.Application.Services.Courses;
-using SkillFlow.Application.Services.CourseSessions;
-using SkillFlow.Application.Services.Locations;
-using SkillFlow.Domain.Interfaces;
 using SkillFlow.Infrastructure;
-using SkillFlow.Infrastructure.Persistence;
-using SkillFlow.Infrastructure.Repositories;
+using SkillFlow.Presentation;
 using SkillFlow.Presentation.Endpoints;
-using SkillFlow.Presentation.Exceptions;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-var enumConverter = new JsonStringEnumConverter();
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.Converters.Add(enumConverter);
-});
-
-builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(enumConverter);
-});
-
+builder.Services.AddPresentation();
 builder.Services.AddApplication();
-
-builder.Services.AddDbContext<SkillFlowDbContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
-
-#region AddScoped
-
-builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork>();
-builder.Services.AddScoped<IAttendeeRepository, AttendeeRepository>();
-builder.Services.AddScoped<IAttendeeQueries, AttendeeRepository>();
-builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-builder.Services.AddScoped<ICourseSessionRepository, CourseSessionRepository>();
-builder.Services.AddScoped<ILocationRepository, LocationRepository>();
-builder.Services.AddScoped<ICompetenceRepository, CompetenceRepository>();
-
-builder.Services.AddScoped<IAttendeeService, AttendeeService>();
-builder.Services.AddScoped<ICourseService, CourseService>();
-builder.Services.AddScoped<ICourseSessionService, CourseSessionService>();
-builder.Services.AddScoped<ILocationService, LocationService>();
-builder.Services.AddScoped<ICompetenceService, CompetenceService>();
-
-#endregion
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("NextJsPolicy", policy =>
-    {
-        policy.WithOrigins("https://localhost:3000",
-            "http://localhost:3000",
-            "https://127.0.0.1:3000",
-            "http://127.0.0.1:3000")
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-    });
-});
+builder.Services.AddInfrastructure(connectionString!);
 
 var app = builder.Build();
 
@@ -85,7 +23,6 @@ if(app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseCors("NextJsPolicy");
 
 app.MapAttenteesEnpoints();
